c27b1ec2f48f7d163d497fc21c4477ffb814dbd9
FahimAIDB <fahim@aidocbuilder.com>
2023-10-07 12:09:27 +0600

    app js updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=d91ca8e6";
+const API_URL = "https://www.omdbapi.com?apikey=d91ca8e6";
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
